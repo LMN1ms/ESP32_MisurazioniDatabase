98c0c88ca86dff36a4b1596cc7eb9109e07fc9c9
LMN1ms <158204735+LMN1ms@users.noreply.github.com>
2024-02-19 22:19:40 +0100

    v2.0
    added telegram notification for connection error.

--- STAZIONEMETEO_IoT/STAZIONEMETEO_IoT.ino
+++ STAZIONEMETEO_IoT/STAZIONEMETEO_IoT.ino
@@ -1,4 +1,4 @@
-//dichiarazione librerie
+// Dichiarazione librerie
 #include <WiFi.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
@@ -6,40 +6,52 @@
 #include <DHT.h>
 #include <HTTPClient.h>
 #include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
 
-//connessione alla rete wifi
+// Connessione alla rete WiFi
 const char *ssid = "IoT_EXT";
 const char *password = "CasaIoT2024|";
 
-//connessione alla pagina web altervista
+// Connessione alla pagina web Altervista
 const char *serverName = "http://managmentalelmn.altervista.org/inviodati.php";
 String apiKeyValue = "tPmAT5Ab3j7F9";
 
-//connessione alla pagina dati thingspeak
+// Connessione alla pagina dati ThingSpeak
 const char *apiKey = "X6GREUFAZIYLU48U";
 
-//pin collegati al sensore BMP180
+// Connessione al bot Telegram
+#define BOTtoken "6861211184:AAEcoKMC2LEoNJ3VQnJs8MdXjbY00dbcxi4"
+#define CHAT_ID "5006934134"
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOTtoken, client);
+int botRequestDelay = 1000;
+unsigned long lastTimeBotRan;
+
+// Pin collegati al sensore BMP180
 const int bmp180SDA = 21;  // SDA
 const int bmp180SCL = 22;  // SCL
-//gestione sensore pressione
+// Gestione sensore pressione
 Adafruit_BMP085 bmp;
 
-//pin collegato al sensore DHT22
+// Pin collegato al sensore DHT22
 #define DHTPIN 4
-//gestione sensore temperatura e umidità
+// Gestione sensore temperatura e umidità
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
 
-//pin collegato al sensore del terreno
+// Pin collegato al sensore del terreno
 #define AOUT_PIN 34
 
+// Dichiarazione variabili a livello globale
+float temperature, humidity, pressure;
 
-//setup
+// Setup
 void setup() {
   Serial.begin(115200);
 
-  //connessione al Wi-Fi
+  // Connessione al WiFi
   WiFi.begin(ssid, password);
+  client.setCACert(TELEGRAM_CERTIFICATE_ROOT);
   while (WiFi.status() != WL_CONNECTED) {
     delay(250);
     Serial.print("NON CONNESSO ");
@@ -47,31 +59,29 @@ void setup() {
   Serial.println("");
   Serial.println("CONNESSO AL WIFI");
 
-  //inizializza il sensore BMP180
+  // Inizializza il sensore BMP180
   if (!bmp.begin()) {
     Serial.println("SENSORE PRESSIONE NON TROVATO!");
     while (1);
   }
 
-  //inizializza il sensore DHT22
+  // Inizializza il sensore DHT22
   dht.begin();
 }
 
 void loop() {
+  // Lettura dati dal sensore DHT22
+  temperature = dht.readTemperature();
+  humidity = dht.readHumidity();
 
-  //lettura dati dal sensore DHT22
-  float temperature = dht.readTemperature();
-  float humidity = dht.readHumidity();
-
-  //lettura dati dal sensore BMP180
-  float pressure = bmp.readPressure() / 100.0F;
+  // Lettura dati dal sensore BMP180
+  pressure = bmp.readPressure() / 100.0F;
   float temperaturebmp = bmp.readTemperature();
 
-  //lettura sensore terreno
+  // Lettura sensore terreno
   int value = analogRead(AOUT_PIN);
 
-
-  //stampa i dati dei sensori nel monitor seriale
+  // Stampa i dati dei sensori nel monitor seriale
   Serial.println("-------------------");
   Serial.println("");
   Serial.print("Temperatura: ");
@@ -101,12 +111,11 @@ void loop() {
   Serial.println("-------------------");
   Serial.println("");
 
-
-  //invio dati a ThingSpeak
+  // Invio dati a ThingSpeak
   if (WiFi.status() == WL_CONNECTED) {
     WiFiClient client;
 
-    //prepara l'URL API
+    // Prepara l'URL API
     String url = "/update?api_key=";
     url += apiKey;
     url += "&field1=";
@@ -120,7 +129,7 @@ void loop() {
     url += "&field5=";
     url += String(moisturePercentage);
 
-    //effettua la richiesta HTTP
+    // Effettua la richiesta HTTP
     if (client.connect("api.thingspeak.com", 80)) {
       Serial.println("Connected to ThingSpeak");
 
@@ -129,61 +138,118 @@ void loop() {
                    "Connection: close\r\n\r\n");
       delay(500);
 
-      //leggi e stampa la risposta
+      // Leggi e stampa la risposta
       while (client.available()) {
         String line = client.readStringUntil('\r');
         Serial.print(line);
       }
 
       Serial.println();
-      Serial.println("Dati inviati a thingspeak");
+      Serial.println("Dati inviati a ThingSpeak");
       Serial.println("");
 
       client.stop();
     } else {
-      Serial.println("Connessione a thingspeak fallita");
+      Serial.println("Connessione a ThingSpeak fallita");
       Serial.println("");
+      bot.sendMessage(CHAT_ID, "Connessione a ThingSpeak fallita");
     }
   }
 
-
-  //invio al database altervista
+  // Invio al database Altervista
   if (WiFi.status() == WL_CONNECTED) {
     WiFiClient client;
     HTTPClient http;
 
-    //inizializza il nome del dominio dove si trova la pagina web
+    // Inizializza il nome del dominio dove si trova la pagina web
     http.begin(client, serverName);
 
-    //tipo del contenuto
+    // Tipo del contenuto
     http.addHeader("Content-Type", "application/x-www-form-urlencoded");
 
-    //invio dei dati con metodo POST
-    String httpRequestData = "api_key=" + apiKeyValue + "&temperatura=" + String(temperature)
-                             + "&umidita=" + String(humidity) + "&pressione=" + String(pressure) + "&umiditaterreno=" + String(moisturePercentage) + "";
+    // Invio dei dati con metodo POST
+    String httpRequestData = "api_key=" + apiKeyValue + "&temperatura=" + String(temperature) +
+                             "&umidita=" + String(humidity) + "&pressione=" + String(pressure) + "&umiditaterreno=" + String(moisturePercentage) + "";
     Serial.print("httpRequestData: ");
     Serial.println(httpRequestData);
 
-
-    //invia il POST http per ricevere eventuali errori
+    // Invia il POST HTTP per ricevere eventuali errori
     int httpResponseCode = http.POST(httpRequestData);
 
     if (httpResponseCode > 0) {
       Serial.print("HTTP codice di risposta: ");
       Serial.println(httpResponseCode);
-    }
-    else {
+    } else {
       Serial.print("Codice errore: ");
       Serial.println(httpResponseCode);
+      bot.sendMessage(CHAT_ID, "Connessione al database fallita.");
     }
-    // Free resources
+
+    // Libera le risorse
     http.end();
-  }
-  else {
+  } else {
     Serial.println("WiFi disconnesso");
   }
 
+  if (millis() > lastTimeBotRan + botRequestDelay) {
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
 
-  //attesa di 120 secondi prima di inviare nuovamente i dati
-  delay(120000);
+    while (numNewMessages) {
+      Serial.println("messaggio ricevuto");
+      handleNewMessages(numNewMessages);
+      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    }
+    lastTimeBotRan = millis();
+  }
+
+  // Attesa di 120 secondi prima di inviare nuovamente i dati
+  setDeepSleep();
+}
+
+void setDeepSleep() {
+  Serial.println("Impostazione della modalità deep sleep...");
+
+  // Configura il tempo di deep sleep in microsecondi
+  // Ad esempio, 1 minuto di sleep
+  esp_sleep_enable_timer_wakeup(120 * 1000000);
+
+  // Alcune configurazioni aggiuntive se necessario
+  // ...
+
+  Serial.println("Entrata in modalità deep sleep...");
+  delay(100); // Attendi per assicurarti che la stampa venga completata
+
+  // Esegui la transizione in modalità deep sleep
+  esp_deep_sleep_start();
+}
+
+String ts, hs, ps;
+
+// Stampiamo sul terminale della seriale indicazioni sul messaggio ricevuto
+void handleNewMessages(int numNewMessages) {
+  Serial.println("Gestione del messaggio");
+  Serial.println(String(numNewMessages));
+  for (int i = 0; i < numNewMessages; i++) {
+    // Verifichiamo che la chat ID sia corretta
+    String chat_id = String(bot.messages[i].chat_id);
+    if (chat_id != CHAT_ID) {
+      bot.sendMessage(chat_id, "Utente non autorizzato", "");
+      continue;
+    }
+
+    String text = bot.messages[i].text;
+    Serial.println(text);
+
+    if (text == "/weather") {
+      ts = String(temperature);
+      bot.sendMessage(chat_id, "temperatura:");
+      bot.sendMessage(chat_id, ts, "");
+      hs = String(humidity);
+      bot.sendMessage(chat_id, "umidita:");
+      bot.sendMessage(chat_id, hs, "");
+      ps = String(pressure);
+      bot.sendMessage(chat_id, "pressione:");
+      bot.sendMessage(chat_id, ps, "");
+    }
+  }
 }
